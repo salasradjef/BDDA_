8c64ee9a519ffbbf284cdfcabf7b397db4d657d7
unknown <radjefsls@gmail.com>
2021-12-04 16:13:41 +0100

    CreateRelationCommand

--- src/CODE/CreateRelationCommand.java
+++ src/CODE/CreateRelationCommand.java
@@ -10,6 +10,18 @@ public class CreateRelationCommand {
     private ColInfo[] col;
 
     public CreateRelationCommand(String command){
+        String[] infos = command.split(" ");
+        this.relName = infos[2]; //set le nom de la relation
+        String  between = infos[3].split("[\\(\\)]")[1];
+        String[] colInfo = between.split(",");
+        this.nbr_col = colInfo.length; //set le nombre de colonne
+
+
+        col = new ColInfo[this.nbr_col];
+        for(int i=0;i<this.nbr_col;i++){
+            String[] colu = colInfo[i].split(":");
+            col[i] = new ColInfo(colu[0],colu[1]);
+        }
 
     }
 
